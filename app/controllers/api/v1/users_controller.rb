86b4b02de066d1ab731c9a2c4a3d4975d445d835
ConditionalStatementLifestyle <github email address>
2019-05-28 09:52:11 -0700

    Changes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create 
-        @user = User.new(user_params)
+        @user = User.find_or_create_by(user_params)
         @user.save
         render json: @user
     end
