0ccb341f4951b1fb52279d225d712c28c34179de
jyang81 <joe.yang.design@gmail.com>
2019-07-26 14:54:45 -0700

    trying middleware approach again, added redistogo on heroku

--- app/middleware/action_cable.rb
+++ app/middleware/action_cable.rb
@@ -1,15 +1,16 @@
 # middleware/action_cable_chat.rb
 
-# class ChatActionCable
-#     def initialize(app, options={})
-#       @app = app
-#     end
-  
-#     def call(env)
-#       if Faye::WebSocket.websocket?(env)
-#         ActionCable.server.call(env)
-#       else
-#         @app.call(env)
-#       end
-#     end
-#   end
+class ChatActionCable
+    def initialize(app, options={})
+      @app = app
+      ActionCable.server.config.allowed_request_origins = ["http://localhost:3000", "https://react-pictionary.herokuapp.com/"]
+    end
+
+    def call(env)
+      if Faye::WebSocket.websocket?(env)
+        ActionCable.server.call(env)
+      else
+        @app.call(env)
+      end
+    end
+  end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # config.middleware.use ChatActionCable
-  config.web_socket_server_url = 'wss://react-pictionary-backend.herokuapp.com/api/v1'
+  config.middleware.use ChatActionCable
+  config.web_socket_server_url = 'wss://react-pictionary-backend.herokuapp.com/api/v1/'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
