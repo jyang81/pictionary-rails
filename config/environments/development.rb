c27405d4494e81b60dff22c0531a2b6a37bfa7c5
jyang81 <joe.yang.design@gmail.com>
2019-08-08 19:33:33 -0700

    works on localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # config.action_cable.url = "wss://react-pictionary-backend.herokuapp.com/cable"
-  config.action_cable.url = "ws://localhost:3000/cable"
+  # config.action_cable.url = "wss://react-pictionary-backend.herokuapp.com/api/v1/cable"
+  config.action_cable.url = "ws://localhost:3000/api/v1/cable"
 
   # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
