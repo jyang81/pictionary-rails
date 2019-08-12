ab4e5b4bbf2f0cdf30134861ced496d87659a04e
github username <github email address>
2019-08-12 14:05:39 -0700

    changed game methods to class methods

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,8 +3,8 @@ class Game < ApplicationRecord
 
     def self.checkForWinner(guess) 
         if self.first.word.downcase == guess["content"].downcase && self.first.drawer_name != guess['user_name']
-            incrementGuesserPoints(guess)
-            incrementDrawerPoints(guess)
+            self.incrementGuesserPoints(guess)
+            self.incrementDrawerPoints(guess)
             ChatMessage.create(
                 content: "Attention please, #{winning_guesser.name} guessed correctly with #{guess['content']}",
                 user_id: 1,
@@ -15,13 +15,13 @@ class Game < ApplicationRecord
         end 
     end
 
-    def incrementGuesserPoints(guess) 
+    def self.incrementGuesserPoints(guess) 
         winning_guesser = User.find(guess['user_id'])
         guesser_games_won = winning_guesser.games_won.to_i + 1
         winning_guesser.update(games_won: guesser_games_won)
     end 
 
-    def incrementDrawerPoints(guess)
+    def self.incrementDrawerPoints(guess)
         game = Game.find(guess["game_id"])
         winning_drawer = User.find(game.drawer_id)
         drawer_games_won = winning_drawer.games_won.to_i + 1
