3283ce60a317a79fc434f949a2e6cf49848189d4
github username <github email address>
2019-08-13 13:56:03 -0700

    check for game in add user

--- app/channels/manager_channel.rb
+++ app/channels/manager_channel.rb
@@ -20,18 +20,20 @@ class ManagerChannel < ApplicationCable::Channel
   def addUserToGame(opts)
     # game = Game.find(opts.fetch('gameId')) # uncomment for multigame
     game = Game.all.first # This is temporary until multi game is implimented
-    users = game.users 
-    users.push(User.where(name: opts.fetch('username')))
-    game.users = users
-    game.save
-    # GameManager.create(command: 'updatedUsers', payload: users)
-    puts "adding user to game here is game info: #{game}"
-    ActionCable
-    .server
-    .broadcast('manager_channel',
-      command: 'updatedUsers',
-      payload: users 
-    )
+    if game
+      users = game.users 
+      users.push(User.where(name: opts.fetch('username')))
+      game.users = users
+      game.save
+      # GameManager.create(command: 'updatedUsers', payload: users)
+      puts "adding user to game here is game info: #{game}"
+      ActionCable
+      .server
+      .broadcast('manager_channel',
+        command: 'updatedUsers',
+        payload: users 
+      )
+    end
   end
 
   def removeUserFromGame(opts)
