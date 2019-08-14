bc7c87be69d833fdf4eb2b58f49e98acdd73cde0
github username <github email address>
2019-08-14 12:20:03 -0700

    return either new game or game in progress when creating

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -6,19 +6,24 @@ class Api::V1::GamesController < ApplicationController
     end
 
     def create
-        @game = Game.new()
-        index = rand(Word.all.length)
-        @game.word = Word.all[index].word
-        @game.drawer_id = params['drawer_id']
-        @game.drawer_name = params['drawer_name']
-        @game.users.push(User.find(params['drawer_id']))
-        @game.save
-        # ManagerChannel.updatedGameState(payload: 'Started')
-        game_command = GameManager.new
-        game_command.command = "updatedGameState"
-        game_command.payload = ["Started"]
-        GameManagerCreationEventBroadcastJob.perform_now(game_command)
-        render json: @game
+        if Game.all.count == 0
+            @game = Game.new()
+            index = rand(Word.all.length)
+            @game.word = Word.all[index].word
+            @game.drawer_id = params['drawer_id']
+            @game.drawer_name = params['drawer_name']
+            @game.users.push(User.find(params['drawer_id']))
+            @game.save
+            # ManagerChannel.updatedGameState(payload: 'Started')
+            game_command = GameManager.new
+            game_command.command = "updatedGameState"
+            game_command.payload = ["Started"]
+            GameManagerCreationEventBroadcastJob.perform_now(game_command)
+            render json: @game
+        end
+        else
+            render json: Game.first
+        end
     end
 
     def destroy
