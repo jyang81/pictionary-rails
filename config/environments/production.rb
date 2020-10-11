4573dcce5ca110433f64d02585f50730c9dae137
jyang81 <joe.yang.design@gmail.com>
2020-10-10 17:43:48 -0700

    Revert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://react-pictionary.herokuapp.com', 'http://react-pictionary.herokuapp.com' ]
 
-  config.action_cable_url = "wss://react-pictionary-backend.herokuapp.com/api/v1/cable"
+  # config.action_cable_url = "wss://react-pictionary-backend.herokuapp.com/api/v1/cable"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,2 @@
-$redis = Redis.new(url: ENV["REDIS_PROVIDER"])
+# $redis = Redis.new(url: ENV["REDIS_URL"])
+$redis = Redis.new(url: 'redis://h:p84c1635e0315343eab147140c48c268a2fbe7e1b18ec22e88dcd9e2ec479789d@ec2-3-220-64-173.compute-1.amazonaws.com:19209')
